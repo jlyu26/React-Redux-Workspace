11ff2888196c6bc73b706f82667b7b2221b36c2a
jlyu-personal <lyujinyan@gmail.com>
2019-01-28 21:15:06 -0500

    What is JSX?

--- react-jsx/jsx/src/index.js
+++ react-jsx/jsx/src/index.js
@@ -4,5 +4,14 @@ import ReactDOM from 'react-dom';
 const App = function() {
     return <div>Hi there!</div>;
 };
+// Equals to: 
+// const App = () => {
+//     return React.createElement(
+//         "div",
+//         null,
+//         "Hi there!"      
+//     );
+// };
+// Convert by babel, JSX is an easier way to write the "behind the scenes" function calls.
 
 ReactDOM.render(<App />, document.querySelector('#root'));
